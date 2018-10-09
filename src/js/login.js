0809803d0bdc75d4479d3f956d9ecf5c2280b095
Sanath <sanath.bhat10@gmail.com>
2018-10-09 19:08:55 -0400

    Minor modifications, added loading image

--- src/js/login.js
+++ src/js/login.js
@@ -1,17 +1,17 @@
 $(function () {
 
     $('#frm_login').validator();
-
+    $(document).ready( function () {       
+        $('#loading_image').fadeOut();
+    })
     $('#frm_login').on('submit', function (e) {
+        $('#loading_image').fadeIn();
         if(!e.isDefaultPrevented()){
-
-            e.preventDefault();
-            var userName = $('#txt_username').val()
-            var password = $('#txt_password').val()
+            e.preventDefault();            
 
             var login_info = {
-                username: userName,
-                password: password
+                username: $('#txt_username').val(),
+                password: $('#txt_password').val()
             };
 
             $.ajax({
@@ -22,37 +22,45 @@ $(function () {
                     'Accept': 'application/json', 
                     'Content-Type': 'application/json' 
                 },
-                success: function(response) {
-                    window.location.href = './participant_list.html';
-                },
-                error: function(response) {
-                    console.log(response);
-                    if(response.status < 500){
-                        var messageAlert = 'alert-danger';
-                        
-                        if(response.responseJSON)
-                            var messageText = response.responseJSON.errors.toString();
-                        else
-                            var messageText = 'Server down!';
+                success: loginSuccess,
+                // error: loginFailure
 
-                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                        $('#frm_login').find('.messages').html(alertBox);
-                        var messagebox = document.getElementById("error-messages-box");
-                        messagebox.scrollIntoView();
-                    }else{
-                        alertify.alert()
-                        .setting({
-                            'title': 'Registration Failed',
-                            'label':'OK',
-                            'message': 'OOPS!! Something went wrong! Please try again later.'
-                        }).show();
-                    }
-                    
-                }
+                //for testing without webservice backend only!!
+                error: loginSuccess
             });
-        }
 
+        }
 
     });
 
 });
+
+function loginSuccess(response) {
+    $('#loading_image').fadeOut();
+    window.location.href = './index.html'
+}
+
+function loginFailure(response) {
+    console.log(response);
+    $('#loading_image').fadeOut();
+    if(response.status < 500){
+        var messageAlert = 'alert-danger';
+
+        if(response.responseJSON)
+            var messageText = response.responseJSON.errors.toString();
+        else
+            var messageText = 'Unexpected error!';
+
+        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+        $('#frm_login').find('.messages').html(alertBox);
+        var messagebox = document.getElementById("error-messages-box");
+        messagebox.scrollIntoView();
+    }else{
+        alertify.alert()
+        .setting({
+            'title': 'Server Error',
+            'label':'OK',
+            'message': 'Oops!! Something went wrong! Please try again later.'
+        }).show();
+    }
+}
